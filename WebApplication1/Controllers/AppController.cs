4de1e85a088ef21022628471da7cd5719e68ad76
samhway <83246475+samhway@users.noreply.github.com>
2022-01-19 17:55:31 -0700

    push to mission3

---
+++ WebApplication1/Controllers/AppController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Grades()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Grades(GradesModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Models/GradesModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class GradesModel
+    {
+
+        [Range(1,100)]
+        public float assign { get; set; }
+        [Range(1, 100)]
+        public float gp { get; set; }
+        [Range(1, 100)]
+        public float quiz { get; set; }
+        [Range(1, 100)]
+        public float exam { get; set; }
+        [Range(1, 100)]
+        public float intex { get; set; }
+
+    }
+}
